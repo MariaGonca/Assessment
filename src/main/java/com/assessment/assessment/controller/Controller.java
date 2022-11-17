29d5202c1bf971cdf2a586c6c5d5fa64aa6e3026
María del Carmen González Carranza <mcgcarranza@hotmail.es>
2022-11-17 13:01:59 +0100

    upload problems

--- src/main/java/com/assessment/assessment/controller/Controller.java
+++ src/main/java/com/assessment/assessment/controller/Controller.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Comment
 @CrossOrigin(origins = {"http://localhost:3000","http://localhost:8080"})
 @RestController
 @RequestMapping("/api")
