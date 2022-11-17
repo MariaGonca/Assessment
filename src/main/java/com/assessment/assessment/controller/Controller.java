86674b3890d2c8342549912e0adbb65523e532fc
María del Carmen González Carranza <mcgcarranza@hotmail.es>
2022-11-17 12:59:56 +0100

    crossorigin changes

--- src/main/java/com/assessment/assessment/controller/Controller.java
+++ src/main/java/com/assessment/assessment/controller/Controller.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","http://localhost:8080"})
 @RestController
 @RequestMapping("/api")
 public class Controller {
